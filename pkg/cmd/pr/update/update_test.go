a8130273ab0a605eb6717308e9b41b51632831fe
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2024-04-09 01:01:19 +0100

    Add tests for `updateRun`
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- pkg/cmd/pr/update/update_test.go
+++ pkg/cmd/pr/update/update_test.go
@@ -2,9 +2,18 @@ package update
 
 import (
 	"bytes"
+	"net/http"
 	"testing"
 
+	"github.com/cli/cli/v2/api"
+	"github.com/cli/cli/v2/context"
+	"github.com/cli/cli/v2/git"
+	"github.com/cli/cli/v2/internal/ghrepo"
+	"github.com/cli/cli/v2/internal/prompter"
+	"github.com/cli/cli/v2/internal/run"
+	shared "github.com/cli/cli/v2/pkg/cmd/pr/shared"
 	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/cli/cli/v2/pkg/httpmock"
 	"github.com/cli/cli/v2/pkg/iostreams"
 	"github.com/google/shlex"
 	"github.com/stretchr/testify/assert"
@@ -141,6 +150,467 @@ func TestNewCmdUpdate(t *testing.T) {
 			assert.Equal(t, tt.output.SkipLocal, gotOpts.SkipLocal)
 			assert.Equal(t, tt.output.UpdateLocal, gotOpts.UpdateLocal)
 			assert.Equal(t, tt.output.Rebase, gotOpts.Rebase)
+		})
+	}
+}
+
+func Test_updateRun(t *testing.T) {
+	defaultInput := func() UpdateOptions {
+		return UpdateOptions{
+			Remotes: func() (context.Remotes, error) {
+				return context.Remotes{
+					{
+						Remote: &git.Remote{
+							Name:     "origin",
+							Resolved: "base",
+						},
+						Repo: ghrepo.New("OWNER", "REPO"),
+					},
+				}, nil
+			},
+			Branch: func() (string, error) {
+				return "pr-branch", nil
+			},
+			Finder: shared.NewMockFinder("123", &api.PullRequest{
+				ID:          "123",
+				HeadRefName: "pr-branch",
+				HeadRefOid:  "head-ref-oid",
+			}, ghrepo.New("OWNER", "REPO")),
+		}
+	}
+
+	tests := []struct {
+		name      string
+		input     *UpdateOptions
+		httpStubs func(*testing.T, *httpmock.Registry)
+		cmdStubs  func(cs *run.CommandStubber)
+		stdout    string
+		stderr    string
+		wantsErr  string
+	}{
+		{
+			name: "success, tty, no update",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Interactive: true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "MERGE", inputs["updateMethod"])
+					}))
+			},
+			stdout: "",
+			stderr: "✓ PR branch already up-to-date\n",
+		},
+		{
+			name: "success, tty, no update, on different local branch",
+			input: &UpdateOptions{
+				Branch: func() (string, error) {
+					return "some-other-branch", nil
+				},
+				SelectorArg: "123",
+				Interactive: true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "MERGE", inputs["updateMethod"])
+					}))
+			},
+			stdout: "",
+			stderr: "✓ PR branch already up-to-date\n",
+		},
+		{
+			name: "success, tty, update (merge), on different local branch",
+			input: &UpdateOptions{
+				Branch: func() (string, error) {
+					return "some-other-branch", nil
+				},
+				SelectorArg: "123",
+				Interactive: true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "new-head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "MERGE", inputs["updateMethod"])
+					}))
+			},
+			stdout: "",
+			stderr: "✓ PR branch updated\n",
+		},
+		{
+			name: "success, tty, update (merge), prompt (no)",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Prompter: &prompter.PrompterMock{
+					ConfirmFunc: func(p string, _ bool) (bool, error) {
+						if p == "Update branch locally?" {
+							return false, nil
+						}
+						return false, prompter.NoSuchPromptErr(p)
+					},
+				},
+				Interactive: true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "new-head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "MERGE", inputs["updateMethod"])
+					}))
+			},
+			stdout: "",
+			stderr: "✓ PR branch updated\n",
+		},
+		{
+			name: "success, tty, update (merge), prompt (yes)",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Prompter: &prompter.PrompterMock{
+					ConfirmFunc: func(p string, _ bool) (bool, error) {
+						if p == "Update branch locally?" {
+							return true, nil
+						}
+						return false, prompter.NoSuchPromptErr(p)
+					},
+				},
+				Interactive: true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "new-head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "MERGE", inputs["updateMethod"])
+					}))
+			},
+			cmdStubs: func(cs *run.CommandStubber) {
+				cs.Register(`git pull .+ origin pr-branch`, 0, "")
+			},
+			stdout: "",
+			stderr: "✓ PR branch updated\n✓ local branch updated\n",
+		},
+		{
+			name: "success, tty, --update-local, update (merge)",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Interactive: true,
+				UpdateLocal: true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "new-head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "MERGE", inputs["updateMethod"])
+					}))
+			},
+			cmdStubs: func(cs *run.CommandStubber) {
+				cs.Register(`git rev-parse --verify refs/heads/pr-branch`, 0, "0000000000000000000000000000000000000000")
+				cs.Register(`git pull .+ origin pr-branch`, 0, "")
+			},
+			stdout: "",
+			stderr: "✓ PR branch updated\n✓ local branch updated\n",
+		},
+		{
+			name: "failure, tty, --update-local, on a different branch",
+			input: &UpdateOptions{
+				Branch: func() (string, error) {
+					return "some-other-branch", nil
+				},
+				SelectorArg: "123",
+				Interactive: true,
+				UpdateLocal: true,
+			},
+			wantsErr: "current branch does not track the PR branch; consider switching to the correct branch or running the command without the `--update-local` option",
+		},
+		{
+			name: "failure, tty, --update-local, no remote pointing at the repo",
+			input: &UpdateOptions{
+				Remotes: func() (context.Remotes, error) {
+					return context.Remotes{
+						{
+							Remote: &git.Remote{
+								Name:     "origin",
+								Resolved: "base",
+							},
+							Repo: ghrepo.New("SOME-OTHER-OWNER", "SOME-OTHER-REPO"),
+						},
+					}, nil
+				},
+				SelectorArg: "123",
+				Interactive: true,
+				UpdateLocal: true,
+			},
+			wantsErr: "current branch does not track the PR branch; consider setting the branch to track the PR branch or running the command without the `--update-local` option",
+		},
+		{
+			name: "failure, tty, --update-local, update (merge), update local branch fails",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Interactive: true,
+				UpdateLocal: true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "new-head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "MERGE", inputs["updateMethod"])
+					}))
+			},
+			cmdStubs: func(cs *run.CommandStubber) {
+				cs.Register(`git rev-parse --verify refs/heads/pr-branch`, 0, "0000000000000000000000000000000000000000")
+				cs.Register(`git pull .+ origin pr-branch`, 1, "some error")
+			},
+			wantsErr: "cannot update local branch; running git pull failed",
+		},
+		{
+			name: "success, tty, --skip-local, update (merge)",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Interactive: true,
+				SkipLocal:   true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "new-head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "MERGE", inputs["updateMethod"])
+					}))
+			},
+			stdout: "",
+			stderr: "✓ PR branch updated\n",
+		},
+		{
+			name: "success, tty, --rebase, no update",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Interactive: true,
+				Rebase:      true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "REBASE", inputs["updateMethod"])
+					}))
+			},
+			stdout: "",
+			stderr: "✓ PR branch already up-to-date\n",
+		},
+		{
+			name: "success, tty, --rebase, update (rebase), with a local branch tracking the remote",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Interactive: true,
+				Rebase:      true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "new-head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "REBASE", inputs["updateMethod"])
+					}))
+			},
+			cmdStubs: func(cs *run.CommandStubber) {
+				cs.Register(`git rev-parse --verify refs/heads/pr-branch`, 0, "0000000000000000000000000000000000000000")
+			},
+			stdout: "",
+			stderr: "✓ PR branch updated\n! warning: due to rebase, you need to manually pull the latest changes to the local branch\n",
+		},
+		{
+			name: "success, tty, --rebase, update (rebase), no local branch tracking the remote",
+			input: &UpdateOptions{
+				SelectorArg: "123",
+				Interactive: true,
+				Rebase:      true,
+			},
+			httpStubs: func(t *testing.T, reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.GraphQL(`mutation PullRequestUpdateBranch\b`),
+					httpmock.GraphQLMutation(`{
+						"data": {
+							"updatePullRequestBranch": {
+								"pullRequest": {
+									"id": "123",
+									"headRefOid": "new-head-ref-oid"
+								}
+							}
+						}
+					}`, func(inputs map[string]interface{}) {
+						assert.Equal(t, "123", inputs["pullRequestId"])
+						assert.Equal(t, "head-ref-oid", inputs["expectedHeadOid"])
+						assert.Equal(t, "REBASE", inputs["updateMethod"])
+					}))
+			},
+			stdout: "",
+			stderr: "✓ PR branch updated\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ios, _, stdout, stderr := iostreams.Test()
+			ios.SetStdoutTTY(true)
+			ios.SetStdinTTY(true)
+			ios.SetStderrTTY(true)
+
+			reg := &httpmock.Registry{}
+			defer reg.Verify(t)
+			if tt.httpStubs != nil {
+				tt.httpStubs(t, reg)
+			}
+
+			if tt.cmdStubs != nil {
+				cs, cmdTeardown := run.Stub()
+				defer cmdTeardown(t)
+				tt.cmdStubs(cs)
+			}
+
+			tt.input.GitClient = &git.Client{
+				GhPath:  "some/path/gh",
+				GitPath: "some/path/git",
+			}
+
+			if tt.input.Remotes == nil {
+				tt.input.Remotes = defaultInput().Remotes
+			}
+
+			if tt.input.Branch == nil {
+				tt.input.Branch = defaultInput().Branch
+			}
+
+			if tt.input.Finder == nil {
+				tt.input.Finder = defaultInput().Finder
+			}
+
+			httpClient := func() (*http.Client, error) { return &http.Client{Transport: reg}, nil }
+
+			tt.input.IO = ios
+			tt.input.HttpClient = httpClient
+
+			err := updateRun(tt.input)
+
+			if tt.wantsErr != "" {
+				assert.EqualError(t, err, tt.wantsErr)
+				return
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, tt.stdout, stdout.String())
+			assert.Equal(t, tt.stderr, stderr.String())
 		})
 	}
 }
